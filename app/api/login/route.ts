d2cf44855b6305af7d960f2f6d31ec8b24834f18
MK-Enock <enockmukwindidza@gmail.com>
2026-06-25 09:59:02 +0200

    [10/06/2026][16:32] Adding requested changes

--- app/api/login/route.ts
+++ app/api/login/route.ts
@@ -28,7 +28,6 @@ export async function POST(request: NextRequest) {
             );
         }
 
-        // Generate token
         const token = uuidv4();
 
         const response = NextResponse.json({
